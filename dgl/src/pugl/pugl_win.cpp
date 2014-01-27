a296a76c708abd089ea589643f6623bde9c0ced8
falkTX <falktx@gmail.com>
2014-01-27 00:16:36 +0000

    Fix win32 build; misc changes

--- dgl/src/pugl/pugl_win.cpp
+++ dgl/src/pugl/pugl_win.cpp
@@ -291,17 +291,11 @@ handleMessage(PuglView* view, UINT message, WPARAM wParam, LPARAM lParam)
 		processMouseEvent(view, 3, false, lParam);
 		break;
 	case WM_MOUSEWHEEL:
-		if (view->scrollFunc) {
-			view->scrollFunc(
-				view, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam),
-				(int16_t)HIWORD(wParam) / (float)WHEEL_DELTA);
-		}
-		break;
 	case WM_MOUSEHWHEEL:
 		if (view->scrollFunc) {
 			view->scrollFunc(
 				view, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam),
-				(int16_t)HIWORD(wParam) / float(WHEEL_DELTA), 0);
+				(int16_t)HIWORD(wParam) / (float)WHEEL_DELTA, 0);
 		}
 		break;
 	case WM_KEYDOWN:
