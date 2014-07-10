0c7e4d2746380f19b0a9bcab278d163316773e36
falkTX <falktx@gmail.com>
2014-07-10 19:06:36 +0100

    Misc

--- distrho/src/DistrhoDefines.h
+++ distrho/src/DistrhoDefines.h
@@ -51,7 +51,7 @@
 # define PROPER_CPP11_SUPPORT
 #elif __cplusplus >= 201103L || (defined(__GNUC__) && defined(__GXX_EXPERIMENTAL_CXX0X__) && (__GNUC__ * 100 + __GNUC_MINOR__) >= 405) || __has_extension(cxx_noexcept)
 # define PROPER_CPP11_SUPPORT
-# if (defined(__GNUC__) && (__GNUC__ * 100 + __GNUC_MINOR__) < 407) && ! __has_extension(cxx_override_control)
+# if (defined(__GNUC__) && (__GNUC__ * 100 + __GNUC_MINOR__) < 407) || (defined(__CLANG__) && ! __has_extension(cxx_override_control))
 #  define override // gcc4.7+ only
 #  define final    // gcc4.7+ only
 # endif
@@ -61,7 +61,7 @@
 # define noexcept throw()
 # define override
 # define final
-# define nullptr (0)
+# define nullptr NULL
 #endif
 
 /* Define DISTRHO_SAFE_ASSERT* */
