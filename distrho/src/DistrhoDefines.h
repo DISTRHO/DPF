f54ab76ad870a0d8089ce590765fd3c015df715c
falkTX <falktx@gmail.com>
2014-08-04 09:50:20 +0100

    clang c++11 fix

--- distrho/src/DistrhoDefines.h
+++ distrho/src/DistrhoDefines.h
@@ -51,7 +51,7 @@
 # define PROPER_CPP11_SUPPORT
 #elif __cplusplus >= 201103L || (defined(__GNUC__) && defined(__GXX_EXPERIMENTAL_CXX0X__) && (__GNUC__ * 100 + __GNUC_MINOR__) >= 405) || __has_extension(cxx_noexcept)
 # define PROPER_CPP11_SUPPORT
-# if (defined(__GNUC__) && (__GNUC__ * 100 + __GNUC_MINOR__) < 407) || (defined(__CLANG__) && ! __has_extension(cxx_override_control))
+# if (defined(__GNUC__) && (__GNUC__ * 100 + __GNUC_MINOR__) < 407 && ! defined(__clang__)) || (defined(__clang__) && ! __has_extension(cxx_override_control))
 #  define override // gcc4.7+ only
 #  define final    // gcc4.7+ only
 # endif
