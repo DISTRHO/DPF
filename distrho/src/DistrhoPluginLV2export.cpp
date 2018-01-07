86bd8079cd3bd665d75edbb986d0c9dbecb1570d
grejppi <grejppi@users.noreply.github.com>
2018-01-07 20:18:07 +0200

    Explicitly state supported LV2 options & use param:sampleRate (#29)
    * Explicitly state supported options and use the correct sampleRate
    
    * Accept lv2:sampleRate too just in case
    
    * Typo
    
    * Style fix
    
    * Remove lv2:sampleRate
    
    * Remove unnecessary newlines
    
    * Missing semicolon

--- distrho/src/DistrhoPluginLV2export.cpp
+++ distrho/src/DistrhoPluginLV2export.cpp
@@ -201,6 +201,7 @@ void lv2_generate_ttl(const char* const basename)
 #ifdef DISTRHO_PLUGIN_BRAND
         pluginString += "@prefix mod:  <http://moddevices.com/ns/mod#> .\n";
 #endif
+        pluginString += "@prefix opts: <" LV2_OPTIONS_PREFIX "> .\n";
         pluginString += "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n";
         pluginString += "@prefix rsz:  <" LV2_RESIZE_PORT_PREFIX "> .\n";
 #if DISTRHO_PLUGIN_HAS_UI
@@ -253,6 +254,12 @@ void lv2_generate_ttl(const char* const basename)
         pluginString += ",\n                        <" MOD_LICENSE__feature "> ";
 #endif
         pluginString += ";\n\n";
+
+        // supportedOptions
+        pluginString += "    opts:supportedOption <" LV2_BUF_SIZE__nominalBlockLength "> ,\n";
+        pluginString += "                         <" LV2_BUF_SIZE__maxBlockLength "> ,\n";
+        pluginString += "                         <" LV2_PARAMETERS__sampleRate "> ;\n";
+        pluginString += "\n";
 
         // UI
 #if DISTRHO_PLUGIN_HAS_UI
@@ -583,8 +590,9 @@ void lv2_generate_ttl(const char* const basename)
         std::fstream uiFile(uiTTL, std::ios::out);
 
         String uiString;
-        uiString += "@prefix lv2: <" LV2_CORE_PREFIX "> .\n";
-        uiString += "@prefix ui:  <" LV2_UI_PREFIX "> .\n";
+        uiString += "@prefix lv2:  <" LV2_CORE_PREFIX "> .\n";
+        uiString += "@prefix ui:   <" LV2_UI_PREFIX "> .\n";
+        uiString += "@prefix opts: <" LV2_OPTIONS_PREFIX "> .\n";
         uiString += "\n";
 
         uiString += "<" DISTRHO_UI_URI ">\n";
@@ -603,7 +611,9 @@ void lv2_generate_ttl(const char* const basename)
         uiString += "\n";
 #  endif
         uiString += "    lv2:requiredFeature <" LV2_OPTIONS__options "> ,\n";
-        uiString += "                        <" LV2_URID__map "> .\n";
+        uiString += "                        <" LV2_URID__map "> ;\n";
+
+        uiString += "    opts:supportedOption <" LV2_PARAMETERS__sampleRate "> .\n";
 
         uiFile << uiString << std::endl;
         uiFile.close();
