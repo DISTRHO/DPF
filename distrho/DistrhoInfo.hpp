c3bf5ecada36bd49f599f20aba63182522ea04cf
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2019-04-06 10:30:12 -0400

    Document DISTRHO_PLUGIN_WANT_FULL_STATE (#135)
    * Document DISTRHO_PLUGIN_WANT_FULL_STATE
    
    * Tweak DISTRHO_PLUGIN_WANT_FULL_STATE summary

--- distrho/DistrhoInfo.hpp
+++ distrho/DistrhoInfo.hpp
@@ -544,6 +544,16 @@ START_NAMESPACE_DISTRHO
 #define DISTRHO_PLUGIN_WANT_STATE 1
 
 /**
+   Wherever the plugin implements the full state API.
+   When this macro is enabled, the plugin must implement a new getState(const char* key) function, which the host calls when saving its session/project.
+   This is useful for plugins that have custom internal values not exposed to the host as key-value state pairs or parameters.
+   Most simple effects and synths will not need this.
+   @note this macro is automatically enabled if a plugin has programs and state, as the key-value state pairs need to be updated when the current program changes.
+   @see Plugin::getState(const char*)
+ */
+#define DISTRHO_PLUGIN_WANT_FULL_STATE 1
+
+/**
    Wherever the plugin wants time position information from the host.
    @see Plugin::getTimePosition()
  */
