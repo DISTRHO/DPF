d83aaf28928bf7a093560fb746f2b4c5160efc51
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2019-07-29 14:02:39 -0400

    Add keycodes support

--- dgl/src/pugl/pugl_x11.c
+++ dgl/src/pugl/pugl_x11.c
@@ -486,6 +486,32 @@ setModifiers(PuglView* view, unsigned xstate, unsigned xtime)
 	view->mods |= (xstate & Mod4Mask)    ? PUGL_MOD_SUPER  : 0;
 }
 
+static unsigned 
+scancodeToHID(unsigned scancode) 
+{
+	const unsigned char KEYCODE_LINUX_TO_HID[256] = {
+    	0,41,30,31,32,33,34,35,36,37,38,39,45,46,42,43,20,26,8,21,23,28,24,12,18,19,
+    	47,48,158,224,4,22,7,9,10,11,13,14,15,51,52,53,225,49,29,27,6,25,5,17,16,54,
+    	55,56,229,85,226,44,57,58,59,60,61,62,63,64,65,66,67,83,71,95,96,97,86,92,
+    	93,94,87,89,90,91,98,99,0,0,100,68,69,0,0,0,0,0,0,0,88,228,84,154,230,0,74,
+    	82,75,80,79,77,81,78,73,76,0,0,0,0,0,103,0,72,0,0,0,0,0,227,231,0,0,0,0,0,0,
+    	0,0,0,0,0,0,118,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    	0,0,0,0,0,0,0,0,0,0,0,0,0,104,105,106,107,108,109,110,111,112,113,114,115,0,
+    	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	};
+
+	//On Linux, the keycodes are offset by KEYCODE_EVDEV_OFFSET when they are sent by evdev
+	const unsigned KEYCODE_EVDEV_OFFSET = 8;
+
+	scancode -= KEYCODE_EVDEV_OFFSET;
+
+    if (scancode >= 256)
+        return 0;
+
+    return KEYCODE_LINUX_TO_HID[scancode];
+}
+
 static void
 dispatchKey(PuglView* view, XEvent* event, bool press)
 {
@@ -515,7 +541,9 @@ dispatchKey(PuglView* view, XEvent* event, bool press)
 			return;
 		}
 	} else if (!special && view->keyboardFunc) {
-		if (view->keyboardFunc(view, press, str[0]) == 0) {
+		const unsigned keycode = scancodeToHID(event->xkey.keycode);
+
+		if (view->keyboardFunc(view, press, str[0], keycode) == 0) {
 			return;
 		}
 	}
