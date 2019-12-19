3f1dfcb0be3ac51fcdf2b0b6a25ef341eb30fa62
CapRat <benjamin.heisch@stud.th-luebeck.de>
2019-12-19 02:29:56 +0100

    Test with MINGW Build (LatencyPlugin)

--- examples/Latency/LatencyExamplePlugin.cpp
+++ examples/Latency/LatencyExamplePlugin.cpp
@@ -249,3 +249,35 @@ Plugin* createPlugin()
 // -----------------------------------------------------------------------------------------------------------
 
 END_NAMESPACE_DISTRHO
+
+#if defined(WIN32) && defined(__MINGW32__)
+#include <windows.h>
+DISTRHO_PLUGIN_EXPORT
+BOOL WINAPI DllMain(
+    HINSTANCE hinstDLL,  // handle to DLL module
+    DWORD fdwReason,     // reason for calling function
+    LPVOID lpReserved)  // reserved
+{
+    // Perform actions based on the reason for calling.
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        // Initialize once for each new process.
+        // Return FALSE to fail DLL load.
+        break;
+
+    case DLL_THREAD_ATTACH:
+        // Do thread-specific initialization.
+        break;
+
+    case DLL_THREAD_DETACH:
+        // Do thread-specific cleanup.
+        break;
+
+    case DLL_PROCESS_DETACH:
+        // Perform any necessary cleanup.
+        break;
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
+#endif
