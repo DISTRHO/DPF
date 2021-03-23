f7b0e9972f26df17414e1214787832390bc1fabf
Martin Bångens <marbangens@gmail.com>
2021-03-23 13:19:31 +0100

    Fix and clean

--- dgl/src/ImageWidgets.cpp
+++ dgl/src/ImageWidgets.cpp
@@ -581,7 +581,7 @@ bool ImageKnob::onMotion(const MotionEvent& ev)
             d     = (ev.mod & kModifierControl) ? 2000.0f : 200.0f;
             if (fRightClicked) d = 2000.0f;
             if (fRightClicked && (ev.mod & kModifierControl)) d = 20000.0f;
-	    value = (fUsingLog ? _invlogscale(fValueTmp) : fValueTmp) + (float(fMaximum - fMinimum) / d * float(movX));
+            value = (fUsingLog ? _invlogscale(fValueTmp) : fValueTmp) + (float(fMaximum - fMinimum) / d * float(movX));
             doVal = true;
         }
     }
@@ -631,8 +631,9 @@ bool ImageKnob::onScroll(const ScrollEvent& ev)
     if (! contains(ev.pos))
         return false;
 
+    const float dir   = (ev.delta.getY() > 0.f) ? 1.f : -1.f;
     const float d     = (ev.mod & kModifierControl) ? 2000.0f : 200.0f;
-    float       value = (fUsingLog ? _invlogscale(fValueTmp) : fValueTmp) + (float(fMaximum - fMinimum) / d * 10.f * ev.delta.getY());
+    float       value = (fUsingLog ? _invlogscale(fValueTmp) : fValueTmp) + (float(fMaximum - fMinimum) / d * 10.f * dir);
 
     if (fUsingLog)
         value = _logscale(value);
