ec35e8e4523117c763d23bd376079048b2b5f4e0
falkTX <falktx@falktx.com>
2021-05-01 16:43:26 +0100

    Rework CI setup
    Signed-off-by: falkTX <falktx@falktx.com>

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,9 +2,17 @@
 
 set -e
 
-sudo add-apt-repository ppa:kxstudio-debian/kxstudio -y
-sudo add-apt-repository ppa:kxstudio-debian/mingw -y
-sudo add-apt-repository ppa:kxstudio-debian/toolchain -y
+# Special macOS native handling
+if [ "${TARGET}" = "macos" ] || [ "${TARGET}" = "macos-universal" ]; then
+    exit 0
+fi
+
+if [ "${TARGET}" = "win32" ] || [ "${TARGET}" = "win64" ]; then
+    wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+    sudo apt-add-repository -y 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+    sudo dpkg --add-architecture i386
+fi
+
 sudo apt-get update -qq
-sudo apt-get install kxstudio-repos
-sudo apt-get update -qq
+sudo apt-get install -y -o APT::Immediate-Configure=false libc6 libc6:i386 libgcc-s1:i386
+sudo apt-get install -y -f
