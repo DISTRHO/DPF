0ddd24b08ba159112681711bfd364e0afd098875
falkTX <falktx@falktx.com>
2021-05-12 13:41:53 +0100

    CI: Use bionic as base distro
    Signed-off-by: falkTX <falktx@falktx.com>

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-sudo add-apt-repository ppa:kxstudio-debian/kxstudio -y
-sudo add-apt-repository ppa:kxstudio-debian/mingw -y
-sudo add-apt-repository ppa:kxstudio-debian/toolchain -y
+sudo add-apt-repository -y ppa:kxstudio-debian/kxstudio
+sudo add-apt-repository -y ppa:kxstudio-debian/mingw
+sudo add-apt-repository -y ppa:kxstudio-debian/toolchain
+
 sudo apt-get update -qq
 sudo apt-get install kxstudio-repos
 sudo apt-get update -qq
