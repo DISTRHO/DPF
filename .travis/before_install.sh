1c7f5093aac8286da68d8fa565cd097cf7e75cde
falkTX <falktx@falktx.com>
2021-05-12 15:02:00 +0100

    CI: Update compiler
    Signed-off-by: falkTX <falktx@falktx.com>

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,6 +5,7 @@ set -e
 sudo add-apt-repository -y ppa:kxstudio-debian/kxstudio
 sudo add-apt-repository -y ppa:kxstudio-debian/mingw
 sudo add-apt-repository -y ppa:kxstudio-debian/toolchain
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
 sudo apt-get update -qq
 sudo apt-get install kxstudio-repos
