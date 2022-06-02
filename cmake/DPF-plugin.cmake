b139002d382e3ed8c36a55d1493e6b0229a03105
falkTX <falktx@falktx.com>
2022-06-02 02:22:52 +0100

    Revert previous commit, enable xcursor for cmake builds
    Signed-off-by: falkTX <falktx@falktx.com>

--- cmake/DPF-plugin.cmake
+++ cmake/DPF-plugin.cmake
@@ -468,9 +468,9 @@ function(dpf__add_dgl_cairo)
   if(NOT APPLE)
     target_sources(dgl-cairo PRIVATE
       "${DPF_ROOT_DIR}/dgl/src/pugl.cpp")
-  else()
-    target_sources(dgl-opengl PRIVATE
-     "${DPF_ROOT_DIR}/dgl/src/pugl.mm")
+  else() # Note: macOS pugl will be built as part of DistrhoUI_macOS.mm
+    #target_sources(dgl-opengl PRIVATE
+    #  "${DPF_ROOT_DIR}/dgl/src/pugl.mm")
   endif()
   target_include_directories(dgl-cairo PUBLIC
     "${DPF_ROOT_DIR}/dgl")
@@ -530,9 +530,9 @@ function(dpf__add_dgl_opengl)
   if(NOT APPLE)
     target_sources(dgl-opengl PRIVATE
       "${DPF_ROOT_DIR}/dgl/src/pugl.cpp")
-  else()
-    target_sources(dgl-opengl PRIVATE
-      "${DPF_ROOT_DIR}/dgl/src/pugl.mm")
+  else() # Note: macOS pugl will be built as part of DistrhoUI_macOS.mm
+    #target_sources(dgl-opengl PRIVATE
+    #  "${DPF_ROOT_DIR}/dgl/src/pugl.mm")
   endif()
   target_include_directories(dgl-opengl PUBLIC
     "${DPF_ROOT_DIR}/dgl")
@@ -596,22 +596,22 @@ function(dpf__add_dgl_system_libs)
     target_include_directories(dgl-system-libs INTERFACE "${X11_INCLUDE_DIR}")
     target_link_libraries(dgl-system-libs INTERFACE "${X11_X11_LIB}")
     target_compile_definitions(dgl-system-libs-definitions INTERFACE "HAVE_X11")
+    if(X11_Xcursor_FOUND)
+      target_link_libraries(dgl-system-libs INTERFACE "${X11_Xcursor_LIB}")
+      target_compile_definitions(dgl-system-libs-definitions INTERFACE "HAVE_XCURSOR")
+    endif()
     if(X11_Xext_FOUND)
       target_link_libraries(dgl-system-libs INTERFACE "${X11_Xext_LIB}")
       target_compile_definitions(dgl-system-libs-definitions INTERFACE "HAVE_XEXT")
-    endif()
-    if(X11_XSync_FOUND)
-      target_link_libraries(dgl-system-libs INTERFACE "${X11_XSync_LIB}")
-      target_compile_definitions(dgl-system-libs-definitions INTERFACE "HAVE_XSYNC")
     endif()
     if(X11_Xrandr_FOUND)
       target_link_libraries(dgl-system-libs INTERFACE "${X11_Xrandr_LIB}")
       target_compile_definitions(dgl-system-libs-definitions INTERFACE "HAVE_XRANDR")
     endif()
-    #if(X11_Xcursor_FOUND)
-    #  target_link_libraries(dgl-system-libs INTERFACE "${X11_Xcursor_LIB}")
-    #  target_compile_definitions(dgl-system-libs-definitions INTERFACE "HAVE_XCURSOR")
-    #endif()
+    if(X11_XSync_FOUND)
+      target_link_libraries(dgl-system-libs INTERFACE "${X11_XSync_LIB}")
+      target_compile_definitions(dgl-system-libs-definitions INTERFACE "HAVE_XSYNC")
+    endif()
    endif()
 
    if(MSVC)
