3aa2e83debdc096fe247829ecdebf11d57ab57e6
falkTX <falktx@falktx.com>
2022-07-04 14:49:21 +0100

    Set VST3 filename for wasm builds
    Signed-off-by: falkTX <falktx@falktx.com>

--- Makefile.plugins.mk
+++ Makefile.plugins.mk
@@ -121,6 +121,8 @@ VST3_FILENAME = $(NAME).vst3/Contents/$(TARGET_PROCESSOR)-linux/$(NAME).so
 else ifeq ($(MACOS),true)
 VST3_CONTENTS = $(NAME).vst3/Contents
 VST3_FILENAME = $(VST3_CONTENTS)/MacOS/$(NAME)
+else ifeq ($(WASM),true)
+VST3_FILENAME = $(NAME).vst3/Contents/wasm/$(NAME).vst3
 else ifeq ($(WINDOWS),true)
 ifeq ($(CPU_I386),true)
 VST3_FILENAME = $(NAME).vst3/Contents/x86-win/$(NAME).vst3
