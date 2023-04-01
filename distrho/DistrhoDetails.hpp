235309a9a42ca6de313facef457a2cbc41afe936
falkTX <falktx@falktx.com>
2023-04-01 21:06:13 +0200

    Different attempt..
    Signed-off-by: falkTX <falktx@falktx.com>

--- distrho/DistrhoDetails.hpp
+++ distrho/DistrhoDetails.hpp
@@ -236,18 +236,18 @@ enum PredefinedPortGroupsIds {
    /**
      Null or unset port group.
     */
-    kPortGroupNone = static_cast<uint32_t>(-1),
+    kPortGroupNone = UINT32_MAX - 1,
 
    /**
      A single channel audio group.
     */
-    kPortGroupMono = static_cast<uint32_t>(-2),
+    kPortGroupMono = UINT32_MAX - 2,
 
    /**
      A 2-channel discrete stereo audio group,
      where the 1st audio port is the left channel and the 2nd port is the right channel.
     */
-    kPortGroupStereo = static_cast<uint32_t>(-3)
+    kPortGroupStereo = UINT32_MAX - 3
 };
 
 /**
