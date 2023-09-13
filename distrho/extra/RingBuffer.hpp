018e45db4a40d57761f12e797093e3699b1ca5e3
falkTX <falktx@falktx.com>
2023-09-13 18:35:33 +0200

    Fix ringbuffer write
    Signed-off-by: falkTX <falktx@falktx.com>

--- distrho/extra/RingBuffer.hpp
+++ distrho/extra/RingBuffer.hpp
@@ -229,7 +229,7 @@ public:
 
         const uint32_t wrap = buffer->tail > buffer->wrtn ? 0 : buffer->size;
 
-        return wrap + buffer->tail - buffer->wrtn;
+        return wrap + buffer->tail - buffer->wrtn - 1;
     }
 
     // -------------------------------------------------------------------
@@ -633,7 +633,7 @@ protected:
         const uint32_t wrtn = buffer->wrtn;
         const uint32_t wrap = tail > wrtn ? 0 : buffer->size;
 
-        if (size > wrap + tail - wrtn)
+        if (size >= wrap + tail - wrtn)
         {
             if (! errorWriting)
             {
