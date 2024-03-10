06843c20353b0f829ddc52a1e5c801d799d343ef
falkTX <falktx@falktx.com>
2024-03-10 14:55:11 +0100

    Fix transient X11 windows
    Signed-off-by: falkTX <falktx@falktx.com>

--- dgl/src/pugl.cpp
+++ dgl/src/pugl.cpp
@@ -363,7 +363,7 @@ PuglStatus puglSetSizeAndDefault(PuglView* view, uint width, uint height)
 
 #ifdef DGL_USING_X11
     // workaround issues in fluxbox, see https://github.com/lv2/pugl/issues/118
-    if (view->impl->win && !view->parent)
+    if (view->impl->win && !view->parent && !view->transientParent)
     {
         view->sizeHints[PUGL_DEFAULT_SIZE].width = view->sizeHints[PUGL_DEFAULT_SIZE].height = 0;
     }
