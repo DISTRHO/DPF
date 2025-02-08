e952d46e897088210a8de66732896ebe518e6d07
falkTX <falktx@falktx.com>
2025-02-08 18:05:44 +0100

    Replace wayland env var workaround with KDE one
    Signed-off-by: falkTX <falktx@falktx.com>

--- dgl/src/pugl.cpp
+++ dgl/src/pugl.cpp
@@ -363,8 +363,8 @@ PuglStatus puglSetSizeAndDefault(PuglView* view, uint width, uint height)
 
 #ifdef DGL_USING_X11
     // workaround issues in fluxbox, see https://github.com/lv2/pugl/issues/118
-    // NOTE troublesome if used under wayland
-    if (view->impl->win && !view->parent && !view->transientParent && std::getenv("WAYLAND_DISPLAY") == nullptr)
+    // NOTE troublesome if used under KDE
+    if (view->impl->win && !view->parent && !view->transientParent && std::getenv("KDE_SESSION_VERSION") == nullptr)
     {
         view->sizeHints[PUGL_DEFAULT_SIZE].width = view->sizeHints[PUGL_DEFAULT_SIZE].height = 0;
     }
