82138d413d004f0e15d7b7cc27f7fc2a2df91734
HITO <39781704+HITOA@users.noreply.github.com>
2026-07-29 19:45:42 +0200

    Fix webview position upon DragEnter event with QT WebEngine (#536)
    * Fix webview position upon DragEnter event with QT WebEngine
    
    * Fix unneeded nesting and missing comment

--- distrho/extra/WebViewImpl.cpp
+++ distrho/extra/WebViewImpl.cpp
@@ -1069,6 +1069,7 @@ struct QEvent {
     uint8_t _[SIZEOF_QEvent];
     enum Type {
         Close = 19,
+        DragEnter = 60,
         ContextMenu = 82,
         User = 1000,
     };
@@ -1848,6 +1849,15 @@ protected:
             }
 
             return true;
+        }
+
+        // HACK forcing webview window position to update, needed for drop area
+        if (type == QEvent::DragEnter && _winId != 0) {
+            XWindowAttributes attrs;
+            XGetWindowAttributes(_display, _winId, &attrs);
+            XMoveWindow(_display, _winId, attrs.x + 1, attrs.y);
+            XMoveWindow(_display, _winId, attrs.x, attrs.y);
+            XFlush(_display);
         }
 
         if (type == QEvent::Close && watched != nullptr && watched == reinterpret_cast<QObject*>(_devToolsView))
